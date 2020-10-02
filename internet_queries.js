ab76468df4f47d6b750c23073872e83e8eeea4c8
evalynledezma <evalynledezmacode@gmail.com>
2020-10-02 13:44:47 -0600

    internet selectors

---
+++ internet_queries.js
@@ -0,0 +1,20 @@
+// Google Search Query
+const searchBar = document.querySelector('.gsfi')
+$('.gsfi').value = '' 
+$('.gsfi').value = 'JavaScript tips'
+$('.jsb').childNodes[0].childNodes
+$('.jsb').childNodes[0].childNodes[0].click
+
+
+// Pull images Instagram
+const images
+const images = document.querySelectorAll('.FFVAD')
+let imageUrlArray = [];
+images.forEach(img => imageUrlArray.push(img.src));
+
+// Auto following accounts and hashtags on linkedin with JavaScript
+let hashtagBtns = document.querySelectorAll('.mn-discovery-hashtag-card__action-btn')
+hashtagBtns.forEach(btn => btn.click())
+const followingBtns = document.querySelectorAll('.follows-recommendation-card__follow-btn')
+followingBtns.forEach(followbtn => followbtn.click())
+
