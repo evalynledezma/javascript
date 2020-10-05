048ec82c8aa88c8334f1823fd201eb6e0576821c
evalynledezma <evalynledezmacode@gmail.com>
2020-10-05 14:01:29 -0600

    equal values

--- modern javascript/basics.js
+++ modern javascript/basics.js
@@ -40,3 +40,44 @@ function fullName (fName, lName) {
   // Arrow function with multiple arguments
   fullName = (fName, lName) => { console.log(`${lName}, ${fName}`); }
   fullName('Kristine', 'Hudgens');
+
+
+  // How to Check if Two Objects Have Equal Values
+  const isEqual = (obj1, obj2) => {
+    const obj1Keys = Object.keys(obj1);
+    const obj2Keys = Object.keys(obj2);
+  
+    if (obj1Keys.length !== obj2Keys.length) {
+      return false;
+    }
+  
+    for (let objKey of obj1Keys) {
+      if (obj1[objKey] !== obj2[objKey]) {
+        return false;
+      }
+    }
+  
+    return true;
+  };
+  
+  const obj1 = {
+    name: "Kristine",
+    age: 13,
+    favorites: {
+      food: "Pizza",
+      vacation: "Disneyland"
+    }
+  };
+  
+  const obj2 = {
+    name: "Kristine",
+    age: 13,
+    favorites: {
+      food: "Pizza",
+      vacation: "Disneyland"
+    }
+  };
+  
+  obj1 == obj2;
+  isEqual(obj1, obj2);
+
