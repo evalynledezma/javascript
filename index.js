eba9f79959de27b80b15b6b37bae103392f6ed96
evalynledezma <evalynledezmacode@gmail.com>
2020-10-05 15:56:36 -0600

    promises

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-var person = prompt ("Please enter your name", " ");
-console.log(person)
+class Home {
+	constructor({ type, payment = "renting" }) {
+		this.type = type;
+		this.payment = payment;
+	}
+
+	static homeImprovement(yourHome) {
+		return (yourHome.payment === 'mortgage')
+	}
+}
+
+const choice1 = new Home ({type: "house", payment: "morgage"})
+console.log(Home.homeImprovement(choice1))
