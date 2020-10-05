e288502c3f2f063f981383b882d5e93b82345746
evalynledezma <evalynledezmacode@gmail.com>
2020-10-05 13:27:50 -0600

    spread and bind

--- deconstruction.js
+++ deconstruction.js
@@ -29,4 +29,15 @@ const apiList = [
   console.log(repos);
   console.log(profile);
 
+
+//   How to Pass JavaScript Objects as Function Arguments by Leveraging Deconstruction
+const user = {
+    name: 'Kristine',
+    email: 'kristine@devcamp.com'
+  }
   
+  const renderUser = ({ name, email }) => {
+    console.log(`${name}: ${email}`);
+  }
+  
+  renderUser(user);
--- functions/arguments.js
+++ functions/arguments.js
@@ -4,3 +4,21 @@ function fullName(firstName, lastName, language) {
 }
 
 console.log(fullName('Jordan', 'Hudgens', 'Spanish'));
+
+
+// Guide to Adding Default Object Values to JavaScript Function Arguments
+const blog = {
+    title: 'My great post',
+    summary: 'Summary of my post'
+  }
+  
+  const openGraphMetadata = ({ title, summary = 'A DailySmarty Post' }) => {
+    console.log(`
+      og-title=${title}
+      og-description=${summary}
+    `);
+  }
+  
+  openGraphMetadata(blog);
+
+  
